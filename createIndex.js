1f681fea1b547c42c9d8529b5718f1e0a7cadbd5
Ruby Adkins <radkins22@students.stab.org>
2021-12-15 23:29:56 -0500

    assimilate binary search + fix some bugs

--- createIndex.js
+++ createIndex.js
@@ -1,16 +1,16 @@
-/* use fs with the commands:
-	let dictionary = fs.readFileSync("./smallDictionary.txt");
-	fs.writeFileSync("./myIndex.txt", "A BIG STRING OF STUFF!");
-*/
 const fs = require('fs');
 
-// Think about your components:
-// first you sort each work to
-// get a alphanumerical word, and
+let dictionary = fs.readFileSync("./bigDictionary.txt").toString().split("\r\n");
 
-// then you sort each word against
-// each other words alphanumerical word
-// Hmm...
-// We wish you well!
+//sort every word in dictionary
+for (let i = 0; i < dictionary.length; i++) {
+	let string = dictionary[i].split("");
+	quickSort(string, 0, string.length);
+	dictionary[i] = string.join("") + " | " + dictionary[i];
+}
 
-console.log("Hey!");
+//sort dictionary
+quickSort(dictionary, 0, dictionary.length-1);
+
+//write to file
+fs.writeFileSync("./myIndex.txt", dictionary.join("\r\n"));
--- queryIndex.js
+++ queryIndex.js
@@ -1,1 +1,128 @@
-Save this for later!
+//QUERYING
+const fs = require('fs');
+
+let dictionary = fs.readFileSync("./myIndex.txt").toString().split("\r\n");
+
+query("pat");
+
+function query(str) {
+	//sort query str
+	str = str.split("");
+	quickSort(str, 0, str.length-1);
+
+	//initialize anagram list
+	let anagrams = [];
+
+	//use binary search get range of words with the same starting value
+	let middleIndex = binarySearch(dictionary, 0, dictionary.length, str);
+	let startingIndex = checkHigherNeighborWords(middleIndex)
+	let afterIndex = checkLowerNeighborWords(middleIndex, dictionary.length);
+
+	//use linear search among the subarr to get our anagram
+	for (let i = startingIndex; i <= afterIndex; i++) {
+		//grab sorted version of dictionary word
+		let sortedword = dictionary[i].split(" | ")[0];
+        let realword = dictionary[i].split(" | ")[1];
+
+		//console.log(sortedword[0], str.join(""));
+		if (sortedword == str.join("")) {
+			//console.log("sexypiggie");
+		
+            anagrams.push(realword);
+		}
+	}
+
+	console.log(anagrams);
+}
+
+function checkHigherNeighborWords(index) {
+	while (index > 0) {
+	//	console.log(index)
+		if (dictionary[index].split("")[0] == dictionary[index-1].split("")[0]) {
+			index = index - 1;
+		}else {
+			return index;
+		}
+	}
+
+	//check base cases
+	if (index == 0) {
+		return index;
+	}
+}
+
+function checkLowerNeighborWords(index, length) {
+	while (index < length-1) {
+	//	console.log(index)
+		if (dictionary[index].split("")[0] == dictionary[index+1].split("")[0]) {
+			index = index + 1;
+		} else {
+			return index;
+		}
+	}
+
+	//check base cases
+	if (index == length-1) {
+		return index;
+	}
+}
+
+//binary search function
+function binarySearch(arr, l, r, x){
+    if (r >= l) {
+        let mid = l + Math.floor((r - l) / 2);
+
+        if (arr[mid].split("")[0] == x[0])
+            return mid;
+
+        if (arr[mid].split("")[0] > x[0])
+            return binarySearch(arr, l, mid - 1, x);
+ 
+        return binarySearch(arr, mid + 1, r, x);
+    }
+
+    return -1;
+}
+
+//quick sort function
+function quickSort(arr, start, end) {
+	if (start < end) {
+		let pivot = partition(arr, start, end);
+
+		//sort each partition based on pivot pos
+		quickSort(arr, start, pivot-1);
+		quickSort(arr, pivot+1, end);
+	} 
+}
+
+function partition(arr, start, end) {
+	//intialize variables of arr indices
+	let p = end;
+	let j = start;
+	let i = j-1;
+
+	//check between j and p while j < p
+	while (j < p) {
+		if (arr[j] < arr[p]) {
+			//increment i by 1
+			i += 1;
+
+			//swap i and j
+			let temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
+		}
+
+		//move j by 1 pos
+		j += 1;
+	}
+
+	//swap between p and i+1
+	let temp = arr[p];
+	arr[p] = arr[i+1];
+	arr[i+1] = temp;
+
+	//change pivot
+	p = i + 1;
+	return p;
+}
